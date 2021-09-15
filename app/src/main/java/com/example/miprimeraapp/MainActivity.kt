1cf8ba0784029b08428bfcbc77952bf5d4593bc6
Miguel <mikidam2afuera@gmail.com>
2021-09-15 11:23:21 +0200

    clase 15 prueba

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, getString(R.string.hello), Toast.LENGTH_LONG).show()
-        Log.v("MainActivity", "Se ha creado correctamente")
+        Log.v("MainActivity", "Se ha creado guay")
     }
 }
